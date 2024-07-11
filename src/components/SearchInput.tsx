84d1b88b4c3e72948e745dd68d23c9bc5ba975b4
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-07-11 14:42:45 +0700

    Refactor- Manage State with Zustand

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,24 +1,30 @@
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
-import { useRef } from 'react'
-import { BsSearch } from 'react-icons/bs'
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
+import useGameQueryStore from "../store";
 
-interface Props {
-  onSearch: (searchText: string) => void;
-}
-
-const SearchInput = ({ onSearch }: Props) => {
+const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
+
   return (
-    <form onSubmit={(event) => {
-      event.preventDefault();
-      if (ref.current) onSearch(ref.current.value);
-    }}>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) setSearchText(ref.current.value);
+      }}
+    >
       <InputGroup>
-          <InputLeftElement children={<BsSearch/>} />
-          <Input ref={ref} borderRadius={20} placeholder='Search games...' variant='filled' />
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
       </InputGroup>
     </form>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { GameQuery } from "./../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 import ms from "ms";
+import useGameQueryStore from "../store";
 
 const apiClient = new APIClient<Game>("/games");
 export interface Game {
@@ -14,8 +14,10 @@ export interface Game {
   rating_top: number;
 }
 
-const useGames = (gameQuery: GameQuery) =>
-  useInfiniteQuery<FetchResponse<Game>, Error>({
+const useGames = () => {
+  const gameQuery = useGameQueryStore((s) => s.gameQuery);
+
+  return useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     queryFn: ({ pageParam = 1 }) =>
       apiClient.getAll({
@@ -30,7 +32,8 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    staleTime: ms('24h')
+    staleTime: ms("24h"),
   });
+};
 
 export default useGames;
