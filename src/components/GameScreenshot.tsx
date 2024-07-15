5012c87f6d1100f3561eda95c491bb9102cabba3
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-07-15 14:45:42 +0700

    Building Game Screenshots

---
+++ src/components/GameScreenshot.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import useScreenshots from '../hooks/useScreenshots';
+import { Image, SimpleGrid } from '@chakra-ui/react';
+
+interface Props {
+    gameId: number;
+}
+
+const GameScreenshot = ({ gameId }: Props) => {
+    const { data, error, isLoading } = useScreenshots(gameId);
+
+    if (isLoading) return null;
+
+    if (error) throw error;
+
+    return (
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+            {data?.results.map(file => <Image key={file.id} src={file.image} />)}
+        </SimpleGrid>
+    )
+}
+
+export default GameScreenshot
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshot from "../components/GameScreenshot";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -19,6 +20,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshot gameId={game.id} />
     </>
   );
 };
